9c05136b242d51cf5bcd91fd032532f587fd035f
anthony <antonsdl@gmail.com>
2018-09-30 21:50:54 +0300

    simplified meta

--- _masters_/Functional programming/metaprogramming.scm
+++ _masters_/Functional programming/metaprogramming.scm
@@ -1,15 +1,24 @@
+(define ie (interaction-environment))
+
+(define (make-name cite postfix) ; symbol-list, str
+  (string->symbol (string-append (symbol->string (car cite)) postfix)))
+
+
 (define (define-struct cite)
-  (let ((predicate `(define (,(string->symbol (string-append (symbol->string (car cite)) "?")) arg)
-                 (eq? ,(car cite) arg))) ; strinbg .comare to symbol
-        (anti-pred `(define (,(string->symbol (string-append (symbol->string (car cite)) "!")) arg)
-                  (not (eq? ,(car cite) arg)))))
-
+  (let ((predicate `(define (,(make-name cite "?") arg)
+                 (equal? ,(symbol->string (car cite)) arg)))
+        (anti-pred `(define (,(make-name cite "!") arg)
+                  (not (equal? ,(symbol->string (car cite)) arg)))))
     (display "eval1")
-    (eval predicate (interaction-environment))
+    (newline)
+    (eval predicate ie)
+    
     (display "eval2")
-    (eval anti-pred (interaction-environment))))
+    (newline)
+    (eval anti-pred ie)))
 
-(define-struct '(point "321"))
+
+(define-struct '(point "ignored-arg"))
 
 (point? "1253")
 (point? "point")
@@ -19,4 +28,5 @@
 
 (point! "1235")
 (point! "point")
+
 
