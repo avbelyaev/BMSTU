5f766d941b8c5ab6d971efe650ee3f665cc7d15a
anthony <antonsdl@gmail.com>
2018-10-13 19:41:06 +0300

    func call works

--- _masters_/Functional programming/interpreter/interp.hs
+++ _masters_/Functional programming/interpreter/interp.hs
@@ -8,7 +8,7 @@ data Env = Env
     { ws :: [String]        -- program
     , counter :: Int            -- word counter
     , stack :: [Int]            -- data stack. осн вычисления здесь
-    , rs :: [Int]           -- return stack. для процедур
+    , retAddrs :: [Int]           -- return stack. для процедур
     , funcs :: [(String, Int)]         -- dictionary (процедура, номер слова)
     } deriving (Show, Read)
 
@@ -17,7 +17,22 @@ isInteger s = case reads s :: [(Integer, String)] of
     [(_, "")] -> True
     _         -> False
 
+
+slice :: Int -> Int -> [a] -> [a]
 slice from to xs = take (to - from + 1) (drop from xs)
+
+
+indexOfItem :: String -> [String] -> Int
+indexOfItem item xs =
+    let eqq = trace ("eq(" ++ show item ++ "," ++ show (head xs) ++ "):" ++ show ((head xs) == item)) 
+                $ item == (head xs)
+    in 
+        if null xs 
+        then error ("could not find " ++ show item ++ " among " ++ show xs)
+        else if eqq
+             then 0
+             else 1 + indexOfItem item (tail xs)
+
 
 
 -- stack grows to the left. that means a,b in (a - b) represented as [b a]
@@ -44,32 +59,50 @@ dupOp e = e { stack = head (stack e) : (stack e) }
 
 
 
+defineCustomFunc e = 
+    let funcName = ws e !! (counter e + 1) -- func name goes after 'define'
+        funcBodyStartIndex = counter e + 2 -- skip 'define', skip <name>
+        modFuncs = funcs e ++ [(funcName, funcBodyStartIndex)] -- add (name, index) to known funcs
 
-{-
-defineFunc env = 
-    let funcName = wc env !! (counter env + 1) -- func name goes after 'define'
-        funcBodyStartIndex = counter env + 2 -- skip 'define', skip <name>
-        funcBodyEndIndex = findNearestItemIndex "end" (wc env) (counter env)
-        modFuncs = funcs env ++ (funcName, funcBodyStartIndex)
-        modCounter = counter env + funcBodyEndIndex
-    in env { counter = modCounter, funcs = modFuncs }
--}
---userDefinedOp opName env = env { stack = modStack, counter = modCounter, rs = modRetStack }
- 
+        restOfProgram = slice (counter e) (length $ ws e) (ws e) -- remove everything to the left
+        funcBodyEndIndex = indexOfItem "end" restOfProgram
+        modCounter = counter e + funcBodyEndIndex
+    in trace ("func: " ++ show funcName ++ 
+                "[" ++ show funcBodyStartIndex ++ ":" ++ show funcBodyEndIndex ++ "]")
+        $ e { counter = modCounter, funcs = modFuncs }
+
+
+callCustomFunc e =
+    let funcNameToCall = ws e !! (counter e)
+        funcNames = map (\fn -> fst fn) (funcs e) -- get names of all known functions
+        funcToCall = funcs e !! (indexOfItem funcNameToCall funcNames) -- find function by name
+        -- get funcAddr from (funcName, funcAddr). sub 1 since next expr in 'interp' is counter++
+        funcCallAddress = snd funcToCall - 1 
+        
+        returnAddr = counter e + 1 -- memorize address where we should return after invocation
+    in e { counter = funcCallAddress, retAddrs = returnAddr : retAddrs e }
+
+
+endCustomFunc e = e { counter = addrToReturn, retAddrs = modRetAddrs }
+    -- get retAddr from top of retAddrs stack. sub 1 since next expr in 'interp' is counter++
+    where addrToReturn = head (retAddrs e) - 1 
+          !modRetAddrs = tail (retAddrs e) -- remove it
+
 
 
 interp :: Env -> Env
 interp e = 
     let cs = "ws:" ++ show (ws e) ++
              " len=" ++ show (length (ws e)) ++
-             " exit=" ++ show (counter e == length (ws e)) ++
-             " counter:" ++ show (counter e) ++
+             " ctr:" ++ show (counter e) ++
+             " funcs:" ++ show (funcs e) ++
+             " ret:" ++ show (retAddrs e) ++
              " stack:" ++ show (stack e) 
     in 
         if (null $ ws e) || (counter e == length (ws e))
         then e
         else let !current = trace cs $ ws e !! counter e -- force this fucker to evaluate
-            in if isInteger current
+            in if isInteger (trace ("curr:" ++ show current) current)
                 then let !newElem = (read current :: Int)
                     in interp e { stack = newElem : (stack e), counter = counter e + 1 }
                 else let !modEnv = case current of
@@ -86,9 +119,10 @@ interp e =
                                 "swap"  -> swapOp e
                                 "dup"   -> dupOp e
 
-                                --"def"   -> defineFunc e
-                                --_       -> userDefinedOp e
-                    in interp modEnv { counter = counter e + 1}
+                                "def"   -> defineCustomFunc e
+                                "end"   -> endCustomFunc e
+                                _       -> callCustomFunc e
+                    in interp modEnv { counter = counter modEnv + 1}
 
 
 
@@ -98,12 +132,13 @@ eva program initStack = interp env
           --      , ("-", subOp)
            --     , ("*", mulOp)
             --    , ("")] 
-        env = Env { ws = words program, counter = 0, stack = initStack, rs = [], funcs = [] }
+        env = Env { ws = words program, counter = 0, stack = initStack, retAddrs = [], funcs = [] }
     
 
-assert prog expected = expected == actualRes
+assert prog expected = (expected == actualRes) && (null actualRetStack)
     where envRes = eva prog []
           actualRes = stack envRes
+          actualRetStack = retAddrs envRes
 
 main = do
     print "start interpreting"
@@ -111,7 +146,9 @@ main = do
     let t1 = assert "1 1 + 1 - 4 * 2 / neg" [-2]
     print t1
 
-    let t2 = assert "def inc 1 + exit" [1]
+    print "============="
+
+    let t2 = assert "def inc 1 + end 2 inc" [3]
     print t2
 
 
