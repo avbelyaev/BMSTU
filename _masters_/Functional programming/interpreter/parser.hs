8c252acf4050b55c46357c0f058a47081d32a4e4
anthony <antonsdl@gmail.com>
2018-12-08 23:40:27 +0300

    added 'UUID' to each node

--- _masters_/Functional programming/interpreter/parser.hs
+++ _masters_/Functional programming/interpreter/parser.hs
@@ -4,13 +4,19 @@ import Text.Read
 import Debug.Trace
 import Data.Maybe
 
+
+import System.IO.Unsafe
+import Data.Unique
+
 import Lexer
+
 
 
 data Node = Node 
     { val       :: String
     , childs    :: [Node]
-    } deriving (Show)
+    , nodeId    :: Int
+    } deriving (Show, Eq)
 
 data Env = Env
     { tokens    :: [Token]
@@ -23,6 +29,9 @@ toInt :: String -> Int
 toInt val = read $ val :: Int
 
 
+-- ================================================
+-- =================  Stack ops  ==================
+-- ================================================
 binOp :: (Int -> Int -> Int) -> Env -> Env
 binOp operation e = e { stack = binOpResult : modStack }
     where n1 = stack e !! 0
@@ -38,60 +47,60 @@ divOp e = binOp (div) e
 pushOp e token = e { stack = intVal : (stack e) }
     where intVal = toInt $ v token
 
-nextToken e = modEnv
-    where nextTok = head $ tokens e
-          modEnv = e { tokens = tail $ tokens e }
+nextToken e = e { tokens = tail $ tokens e }
 
 
--- pretty printer funcs
 
-makeGenericNode val = Node { val = val, childs = [] }
 
-makeEmptyNode = makeGenericNode "e"
 
-makeNodeE e = e { nodes = newNode : modNodes }
+-- ================================================
+-- =================  Node ops  ===================
+-- ================================================
+uid uniqueId = hashUnique $ unsafePerformIO uniqueId
+
+
+makeLeafNode val nodeId = Node { val = val, childs = [], nodeId = nodeId }
+
+
+makeBinaryNode val e = e { nodes = newNode : modNodes }
     where modNodes = tail $ tail $ nodes e
-          nodeT  = nodes e !! 0
-          nodeE1 = nodes e !! 1
-          newNode = Node { val = "E", childs = [nodeT, nodeE1]}
+          n1 = nodes e !! 0
+          n2 = nodes e !! 1
+          newNode = Node { val = val
+                         , childs = [n1, n2]
+                         , nodeId = uid newUnique }
 
-makeNodeT e = e { nodes = newNode : modNodes }
+
+makeTernaryNode val extraNodeVal e = e { nodes = newNode : modNodes }
     where modNodes = tail $ tail $ nodes e
-          nodeF  = nodes e !! 0
-          nodeT1 = nodes e !! 1
-          newNode = Node { val = "T", childs = [nodeF, nodeT1]}
+          n1 = nodes e !! 0
+          n2 = nodes e !! 1
+          nExtra = makeLeafNode extraNodeVal (uid newUnique)
+          newNode = Node { val = val
+                         , childs = [nExtra, n1, n2]
+                         , nodeId = uid newUnique }
 
-makeNodeE1 e = e { nodes = newNode : modNodes }
-    where modNodes = tail $ tail $ nodes e
-          nodeT  = nodes e !! 0
-          nodeE1 = nodes e !! 1
-          nodeOp = makeGenericNode "(+/-)"
-          newNode = Node { val = "E1", childs = [nodeOp, nodeT, nodeE1] }
 
-makeNodeE1Empty e = e { nodes = newNode : (nodes e) }
-    where newNode = Node { val = "E1", childs = [makeEmptyNode] }
+makeNodeWithChild val childVal e = e { nodes = newNode : (nodes e) }
+    where child = makeLeafNode childVal (uid newUnique)
+          newNode = Node { val = val
+                         , childs = [child]
+                         , nodeId = uid newUnique }
 
-makeNodeT1 e = e { nodes = newNode : modNodes }
-    where modNodes = tail $ tail $ nodes e
-          nodeF  = nodes e !! 0
-          nodeT1 = nodes e !! 1
-          nodeOp = makeGenericNode "(*/:)"
-          newNode = Node { val = "T1", childs = [nodeOp, nodeF, nodeT1] }
 
-makeNodeT1Empty e = e { nodes = newNode : (nodes e) }
-    where newNode = Node { val = "T1", childs = [makeEmptyNode] }
-
-makeNodeF token e = e { nodes = newNode : (nodes e) }
-    where nodeVal = makeGenericNode $ v token
-          newNode = Node { val = "F", childs = [nodeVal] }
-
-makeNodeFBracket e = e { nodes = newNode : modNodes }
+makeNodeFBracket e = e { nodes = newNode : modNodes}
     where modNodes = tail $ nodes e
-          nodeE = nodes e !! 0
-          nodeLeftParen = makeGenericNode "("
-          nodeRightParen = makeGenericNode ")"
-          newNode = Node { val = "F", childs = [nodeLeftParen, nodeE, nodeRightParen] }
+          nE = nodes e !! 0
+          nLeftParen = makeLeafNode "(" (uid newUnique)
+          nRightParen = makeLeafNode ")" (uid newUnique)
+          newNode = Node { val = "F"
+                         , childs = [nLeftParen, nE, nRightParen]
+                         , nodeId = uid newUnique }
 
+
+-- ================================================
+-- =================   Parser   ===================
+-- ================================================
 -- <E> ::= <T> <E’>
 -- <T> ::= <F> <T’>
 -- <E’> ::= + <T> <E’> | - <T> <E’> | e
@@ -102,12 +111,12 @@ makeNodeFBracket e = e { nodes = newNode : modNodes }
 -- <E> ::= <T> <E’>
 parse_E :: Env -> Env
 parse_E e = 
-    makeNodeE $ parse_E1 $ parse_T e
+    makeBinaryNode "E" (parse_E1 $ parse_T e)
 
 -- <T> ::= <F> <T’>.
 parse_T :: Env -> Env
 parse_T e =
-    makeNodeT $ parse_T1 $ parse_F e
+    makeBinaryNode "T" (parse_T1 $ parse_F e)
 
 -- <E’> ::= + <T> <E’> | - <T> <E’> | e
 parse_E1 :: Env -> Env
@@ -115,9 +124,9 @@ parse_E1 e =
     let token = head $ tokens e
         op = v $ token
     in case op of
-        "+" -> makeNodeE1 $ parse_E1 $ addOp $ parse_T $ nextToken e
-        "-" -> makeNodeE1 $ parse_E1 $ subOp $ parse_T $ nextToken e
-        _   -> makeNodeE1Empty e
+        "+" -> makeTernaryNode "E1" "+" (parse_E1 $ addOp $ parse_T $ nextToken e)
+        "-" -> makeTernaryNode "E1" "-" (parse_E1 $ subOp $ parse_T $ nextToken e)
+        _   -> makeNodeWithChild "E1" "e" e
 
 -- <T’> ::= * <F> <T’> | / <F> <T’> | e
 parse_T1 :: Env -> Env
@@ -125,17 +134,18 @@ parse_T1 e =
     let token = head $ tokens e
         op = v $ token
     in case op of
-        "*" -> makeNodeT1 $ parse_T1 $ mulOp $ parse_F $ nextToken e
-        "/" -> makeNodeT1 $ parse_T1 $ divOp $ parse_F $ nextToken e
-        _   -> makeNodeT1Empty e
+        "*" -> makeTernaryNode "T1" "*" (parse_T1 $ mulOp $ parse_F $ nextToken e)
+        "/" -> makeTernaryNode "T1" "/" (parse_T1 $ divOp $ parse_F $ nextToken e)
+        _   -> makeNodeWithChild "T1" "e" e
 
 -- <F> ::= <number> | <var> | ( <E> ) | - <F>
 parse_F :: Env -> Env
 parse_F e =
     let token = head $ tokens e
         tokType = t $ token
+        value = v $ token
     in case tokType of
-        NumberToken     -> makeNodeF token (nextToken $ pushOp e token)
+        NumberToken     -> makeNodeWithChild "F" value (nextToken $ pushOp e token)
         BracketToken    -> makeNodeFBracket $ nextToken $ parse_E $ nextToken e
         _               -> error "unexpected token"
 
@@ -148,22 +158,62 @@ parse tokenList = parse_E e
                   , nodes = [] }
 
 
+printDigraph :: [Node] -> String
+printDigraph nodes = graphvizHeader ++ nodeList ++ edgeList ++ graphvizEnder
+    where graphvizHeader = "digraph {           \n\
+          \  rankdir = LR                       \n\
+          \  dummy [label = '', shape = none]   \n"
+          graphvizEnder = "}"
+          nodeList = "\n"
+          edgeList = "  <edges>\n"
+
+
+mydfs graph visited [] = reverse visited
+mydfs graph visited (x:xs) | elem x visited = mydfs graph visited xs
+                           | otherwise = mydfs graph (x:visited) ((graph !! x) ++ xs)
+
+
+dfs :: [Node] -> [Node] -> [Node]
+dfs visited nodes =
+    if null $ nodes
+    then reverse visited
+    else let curr = head nodes
+             adjacent = childs curr
+         in if curr `elem` visited
+            then dfs visited (tail nodes)
+            else dfs (curr:visited) (nodes ++ adjacent)
+
+
+
 -- run Parser after Lexer: 
 -- - make sure Lexer's filename is lexer.hs,
 -- - comment Lexer's `main`
 -- - do `runhaskell parser.hs`
 main = do
-    --let p2 = "(5 - 1) * (2 + 1) "
+    print ">>> scanning"
+    -- let p2 = "(5 - 1) * (2 + 1) "
     -- let p2 = "2 * 5 + 3 * 4 + 4 * 6 "
-    let p2 = "1 + 2 "
+    -- let p2 = "1 + 2 "
+    let p2 = "1 + 2 + 3 * 4 - 5 "
     let tokens = scan p2
     mapM_ print tokens
 
-    print "start parsing"
+    print ">>> parsing"
     let res = parse tokens
+
+    print ">>> result"
     print $ stack res
 
-    print $ nodes res
-    
+    print ">>> AST"
+    let ast = nodes res
+    print ast
+
+    putStr $ printDigraph ast
+
+
+    print ">>> DFS"
+    let vis = dfs [] ast
+    mapM_ print vis
+
 
     
