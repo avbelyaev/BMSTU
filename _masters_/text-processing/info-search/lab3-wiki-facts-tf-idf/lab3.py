c8727dbf05e8aa8392b4bee4492e6036aa141cab
anthony <antonsdl@gmail.com>
2019-02-26 02:13:00 +0300

    refactored TFIDF

--- _masters_/text-processing/info-search/lab3-wiki-facts-tf-idf/lab3.py
+++ _masters_/text-processing/info-search/lab3-wiki-facts-tf-idf/lab3.py
@@ -41,12 +41,6 @@ class Vectorizable:
         for term in VOCABULARY:
             self.vector[term] = self.sentence.count(term)  # count == TermFrequency
 
-    def norm(self) -> float:
-        n = 0
-        for word in self.vector.keys():
-            n += self.vector[word] ** 2
-        return math.sqrt(n)
-
     def __hash__(self):
         return hash(self.sentence)
 
@@ -76,31 +70,39 @@ def clean_up_sentence(s: str) -> str:
 
 def read_sentences(filename: str) -> list:
     text = open(filename, 'r').read()
-    sentences = nltk.sent_tokenize(text)                                        # split into sentences
-    clean_sentences = list(map(lambda s: clean_up_sentence(s), sentences))      # clean up
-    normalized = list(map(lambda s: normalize_sentence(s), clean_sentences))    # normalize words
+    preprocessed = re.sub(r'[,:;-]', ' ', text)
+    sentences = nltk.sent_tokenize(preprocessed)                                # split into sentences
+    clean_sents = list(map(lambda s: clean_up_sentence(s), sentences))          # clean up
+    no_short_sents = list(filter(lambda s: len(s.split(' ')) >= 3, clean_sents))# remove short sentences
+    normalized = list(map(lambda s: normalize_sentence(s), no_short_sents))     # normalize words
     return normalized
 
 
 def scalar_product(v1: dict, v2: dict):
-    # make sure vectors have same dimension
-    assert len(v1) == len(v2)
     p = 0
     for w in v1.keys():
         p += v1[w] * v2[w]
     return p
 
 
+def norm(vect: dict) -> float:
+    n = 0
+    for word in vect.keys():
+        n += vect[word] ** 2
+    return math.sqrt(n)
+
+
 def vector_space_model(query: Vectorizable, docs: list) -> list:
     cos_similarity = {}
     for doc in docs:
-        cos_similarity[doc] = scalar_product(query.vector, doc.vector) / (query.norm() * doc.norm())
+        multiplied_norms = norm(query.vector) * norm(doc.vector)
+        cos_similarity[doc] = scalar_product(query.vector, doc.vector) / multiplied_norms
 
     sorted_by_weight = sorted(cos_similarity.items(), key=lambda doc: doc[1], reverse=True)
     return sorted_by_weight[:RESULTS_TO_OUTPUT]
 
 
-def tf_idf(query: Vectorizable, docs: list):
+def tf_idf(query: Vectorizable, docs: list) -> list:
     # TF == frequency of a term in a document or query (both are Vectorizable)
     def tf(term: str, vect: Vectorizable) -> int:
         return vect.words.count(term)
@@ -109,31 +111,31 @@ def tf_idf(query: Vectorizable, docs: list):
     def idf(term: str) -> float:
         containing_term = list(filter(lambda doc: term in doc.words, docs))
         df = len(containing_term)
+        if 0 == df: df = 1
         return math.log(len(docs) / df)
 
-    recalculated_doc_weights = {}
+    # count TFIDF weight for each document
+    tfidf_doc_weights = {}
     for doc in docs:
         weight = defaultdict(int)
         for word in doc.words:
             weight[word] = tf(word, doc) * idf(word)
-        recalculated_doc_weights[doc] = weight
+        tfidf_doc_weights[doc] = weight
 
-    tip = list(recalculated_doc_weights)[:30]
-
+    # count TFIDF for a query
+    tfidf_query_weight = defaultdict(int)
     for word in query.words:
-        query_weight[word] = tf(word, query) * idf(word)
+        tfidf_query_weight[word] = tf(word, query) * idf(word)
 
-    # now that we have constant weight of query, we can count weight of each document
-    doc_weight = {}
+    # now just apply cosine similarity like for vector-space model
+    cos_similarity = {}
     for doc in docs:
-        doc_weight[doc]
+        doc_weight = tfidf_doc_weights[doc]
+        multiplied_norms = norm(tfidf_query_weight) * norm(doc_weight)
+        cos_similarity[doc] = scalar_product(tfidf_query_weight, doc_weight) / multiplied_norms
 
-    for doc in docs:
-
-        query_weight = []
-        for word in query.words:
-            query_weight[word] = tf(word, doc)
-
+    sorted_by_weight = sorted(cos_similarity.items(), key=lambda doc: doc[1], reverse=True)
+    return sorted_by_weight[:RESULTS_TO_OUTPUT]
 
 
 def main():
@@ -141,27 +143,28 @@ def main():
     for article_filename in ARTICLES:
         sentences.extend(read_sentences(article_filename))
 
-    # remove sentences that are too short (less than 3 words)
-    sentences = list(filter(lambda s: len(s.split(' ')) >= 3, sentences))
-
     # create vocabulary from words of all sentences
     for sentence in sentences:
-        words = sentence.split(' ')
-        VOCABULARY.update(words)
+        VOCABULARY.update(sentence.split(' '))
 
     # remove empty word that could appear by mistake :)
     if '' in VOCABULARY:
         VOCABULARY.remove('')
 
-    docs = list(map(lambda s: Vectorizable(s), sentences))
-    for d in docs:
-        d.vectorize()
-
     clean_queries = list(map(lambda s: clean_up_sentence(s), FACTS))            # clean up
     norm_queries = list(map(lambda s: normalize_sentence(s), clean_queries))    # normalize
-    queries = list(map(lambda q: Vectorizable(q), norm_queries))
 
-    # find most relevant docs with vector-space model (no IDF)
+    # add words from query to dictionary
+    for q in norm_queries:
+        VOCABULARY.update(q.split(' '))
+
+    queries = list(map(lambda q: Vectorizable(q), norm_queries))
+    [q.vectorize() for q in queries]
+
+    docs = list(map(lambda s: Vectorizable(s), sentences))
+    [d.vectorize() for d in docs]
+
+    print('Vector space model')
     for q in queries:
         q.vectorize()
         matched_docs = vector_space_model(q, docs)
@@ -169,14 +172,12 @@ def main():
         for match in matched_docs:
             print(f'\t{match[1]:.3f} {match[0]}')
 
-    # -//- with TF-IDF model
+    print('TF-IDF')
     for q in queries:
         matched_docs = tf_idf(q, docs)
         print(q)
         for match in matched_docs:
             print(f'\t{match[1]:.3f} {match[0]}')
-
-    print('asdsa')
 
 
 if __name__ == '__main__':
