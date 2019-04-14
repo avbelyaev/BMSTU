384f6c042686dec3e1e8180d39589140a9de4d17
anthony <antonsdl@gmail.com>
2019-04-14 23:20:16 +0300

    added image convolutions and conv plots

--- _masters_/Image Processing/lab-1-convolution/task2imgFilter.m
+++ _masters_/Image Processing/lab-1-convolution/task2imgFilter.m
@@ -7,19 +7,27 @@ img = imread('BioID_0003.pgm');
 % 3x3 kernels
 sobelm_x = [ 1 2 1; 0 0 0; -1 -2 -1];
 sobelm_y = [ -1 0 1; -2 0 2; -1 0 1];
+
 prewit_x = [ -1 1 1; -1 -2 1; -1 1 1];
 prewit_y = [ 1 1 1; -1 -2 1; -1 -1 1];
+
 kirsch_x = [ -3 -3 5; -3 0 5; -3 -3 5];
 kirsch_y = [ -3 5 5; -3 0 5; -3 -3 -3];
+
 robin3_x = [ -1 0 1; -1 0 1; -1 0 1];
 robin3_y = [ 0 1 1; -1 0 1; -1 -1 1];
+
 robin5_x = [ -1 0 1; -2 0 2; -1 0 1];
 robin5_y = [ 0 1 2; -1 0 1; -2 1 0];
     
 
-w_x = conv2(img, robin5_x); 
-w_y = conv2(img, robin5_y);
+w_x = conv2(img, sobelm_x); 
+w_y = conv2(img, sobelm_y);
 W = sqrt(w_x.^2 + w_y.^2); % wiki formula
 
-imwrite(img, gray(256), 'origin.png');
+imwrite(W, gray(256), 'sobel.png');
 % imshow(W, []);
+
+f = figure();
+plot(W);
+saveas(gcf, 'sobel-p.png');
