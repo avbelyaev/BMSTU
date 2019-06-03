11795f408cafa4733e047ce729bb08a443332bb4
anthony <antonsdl@gmail.com>
2019-06-04 00:54:28 +0300

    have no idea what i am doing

--- Object-Oriented Programming/mod2019/excel/excel.cpp
+++ Object-Oriented Programming/mod2019/excel/excel.cpp
@@ -1,72 +1,123 @@
 #include <iostream>
+#include <map>
+
 
 using namespace std;
 
 
-template <class T>
+//template <class T>
+class Formula {
+public:
+    int arg1;
+    int arg2;
+    string op;
+
+    Formula(int arg1_, int arg2_, const string& op_) {
+        arg1 = arg1_;
+        arg2 = arg2_;
+        op = op_;
+    }
+};
+
+
+//template <class T>
 class Cell {
 
 
 public:
-    T data;
+    int data;
+    bool isData;
+    Cell* c1;
+    Cell* c2;
+    string op;
 
-    Cell() {
-        // empty
+    Cell(int value_) {
+        data = value_;
+        isData = true;
     }
 
-    Cell<T>& operator= (const Cell<T>& other_cell) {
+    Cell(Cell& c1_, Cell& c2_, string op_) {
+        c1 = &c1_;
+        c2 = &c2_;
+        op = op_;
+        isData = false;
+    };
+
+    Cell(Cell& c1_, int value, string op_) {
+        c1 = &c1_;
+        op = op_;
+        isData = false;
+    };
+
+    int calc() {
+        if (isData) {
+            return data;
+        }
+        if ("+" == op) {
+            return c1->calc() + c2->calc();
+        }
+        return 0;
+    }
+
+    Cell& operator= (const Cell& other_cell) {
         data = other_cell.data;
         return *this;
     }
 
-    Cell<T>& operator= (const T other_data) {
+    Cell& operator= (int other_data) {
         data = other_data;
         return *this;
     }
 
+    friend Cell& operator+ (Cell& arg1, Cell& arg2) {
+        Cell* c3 = new Cell(arg1, arg2, "+");
+//        Cell c3(arg1, arg2, "+");
+        return *c3;
+    }
+
+//    friend Cell& operator/ (const Cell& c, int value) {
+//        Cell* c3 = new Cell(c, value, "/");
+//        return *c3;
+//    }
 };
 
 
 
-
-
-template <class T>
+//template <class T>
 class SuperCalc {
-    int rows, cols;
-    Cell<T> **data;
+    map<int, Cell> data;
 
 public:
-    SuperCalc<T>(int rows_, int cols_) {
-        rows = rows_;
-        cols = cols_;
-
-        data = new Cell<T> *[rows];
-        for(int i = 0; i < rows; ++i){
-
-            data[i] = new Cell<T> [cols];
-            for(int j = 0; j < cols; ++j){
-                data[i][j] = *(new Cell<T>());
-            }
-        }
+    SuperCalc(int rows_, int cols_) {
+//        map<int, int> m;
     };
 
-    Cell<T>& operator() (int row, int col) {
-        return data[row][col];
-    };
+//    Cell& operator() (int row, int col) {
+//        return data[new pair(row, col)];
+//    };
 };
 
 
 int main() {
 
-    SuperCalc<int> s(1, 10);
-    Cell<int> c1 = s(0, 10);
+//    SuperCalc s(1, 10);
+//    Cell c1 = s(0, 10);
+//
+//    cout << c1.data << endl;
+//
+//    s(0, 10) = 5;
+//    c1 = s(0, 10);
+//    cout << c1.data << endl;
 
-    cout << c1.data << endl;
+    Cell c1(1);
+    Cell c2(2);
 
-    s(0, 10) = 5;
-    c1 = s(0, 10);
-    cout << c1.data << endl;
+    Cell c3 = c1 + c2;
+    cout << c3.calc() << endl;
 
+    c1 = 5;
+
+    cout << c3.calc() << endl;
 
     return 0;
 }
