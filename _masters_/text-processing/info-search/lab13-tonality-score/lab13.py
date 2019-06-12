5ae2d2517dd1dc520cb1296343f5cb09e8e2a539
anthony <antonsdl@gmail.com>
2019-06-12 13:13:45 +0300

    normalization works

--- _masters_/text-processing/info-search/lab13-tonality-score/lab13.py
+++ _masters_/text-processing/info-search/lab13-tonality-score/lab13.py
@@ -1,14 +1,18 @@
 from xml.dom import minidom
 
+import nltk
+from nltk.corpus import stopwords
+
+from lab16 import clean_up_sentence, normalize_sentence
 
 TRAIN_DATA = 'news_eval_train.xml'
+TRAIN_DATA_2 = 'news_eval_train_SHORT.xml'
 TEST_DATA = 'news_eval_test.xml'
 
+TONALITY = ['+', '-', '0']
 
-TONE_POS = '+'
-TONE_NEG = '-'
-TONE_NEUT = '0'
-TONALITY = [TONE_POS, TONE_NEG, TONE_NEUT]
+nltk.download('stopwords')
+STOP_WORDS = stopwords.words('russian')
 
 
 class Cite:
@@ -17,12 +21,34 @@ class Cite:
         self.speech = speech.strip()
         self.evaluation = evaluation.strip()
         self.url = url.strip()
+        self.tokenized = tokenize(self.speech)
 
     def __str__(self):
         return f'{self.evaluation}: {self.speech}'
 
     def __repr__(self):
         return self.__str__()
+
+
+# [[1, 2, 3], [4], [5, 6]] -> [1, 2, 3, 4, 5, 6]
+def flatten(lst: list) -> list:
+    flat_list = []
+    for sublist in lst:
+        for item in sublist:
+            flat_list.append(item)
+    return flat_list
+
+
+def tokenize(text: str) -> list:
+    def remove_stopwords(sentence: str) -> list:
+        words = sentence.split(' ')
+        return list(filter(lambda w: w not in STOP_WORDS, words))
+
+    sentences = nltk.sent_tokenize(text)
+    clean_sents = list(map(lambda s: clean_up_sentence(s), sentences))
+    norm_sents = list(map(lambda s: normalize_sentence(s), clean_sents))
+    no_stopwords_sents = list(map(lambda s: remove_stopwords(s), norm_sents))
+    return flatten(no_stopwords_sents)
 
 
 def parse(filename: str) -> list:
@@ -43,13 +69,13 @@ def parse(filename: str) -> list:
 
 
 def main():
-    all_train = parse(TRAIN_DATA)
+    all_train = parse(TRAIN_DATA_2)
     train_cites = list(filter(lambda cite: cite.evaluation in TONALITY, all_train))
     print(f'train: {len(all_train)} -> {len(train_cites)}')
-
-    all_test = parse(TEST_DATA)
-    test_cites = list(filter(lambda cite: cite.evaluation in TONALITY, all_test))
-    print(f'test : {len(all_test)} -> {len(test_cites)}')
+    #
+    # all_test = parse(TEST_DATA)
+    # test_cites = list(filter(lambda cite: cite.evaluation in TONALITY, all_test))
+    # print(f'test : {len(all_test)} -> {len(test_cites)}')
 
 
 if __name__ == '__main__':
