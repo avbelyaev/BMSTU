b0fe1f13fdcf546eb909b8e6a0846ec806121b43
anthony <antonsdl@gmail.com>
2019-09-12 01:23:01 +0300

    private fields

--- _masters_/ml/image-edit/editor.py
+++ _masters_/ml/image-edit/editor.py
@@ -17,11 +17,11 @@ class RotationPoint(Enum):
 
 class ImgEditor:
     def __init__(self, img_path: str):
-        self.img_path = img_path
-        self.img = cv2.imread(img_path)
-        self.rows = self.img.shape[0]
-        self.cols = self.img.shape[1]
-        self.shape = (self.cols, self.rows)
+        self._img_path = img_path
+        self._img = cv2.imread(img_path)
+        self._rows = self._img.shape[0]
+        self._cols = self._img.shape[1]
+        self._shape = (self._cols, self._rows)
 
     def rotate(self, over: RotationPoint = RotationPoint.CENTER, angle: int = 0):
         """
@@ -29,39 +29,39 @@ class ImgEditor:
         """
         point = self._get_rotation_point(over)
         rot_matrix = cv2.getRotationMatrix2D(point, angle, 1)
-        self.img = cv2.warpAffine(self.img, rot_matrix, self.shape)
+        self._img = cv2.warpAffine(self._img, rot_matrix, self._shape)
 
     def shrink(self, x_factor: float = 1.0, y_factor: float = 1.0):
-        self.img = cv2.resize(self.img, None,
-                              fx=x_factor, fy=y_factor,
-                              interpolation=cv2.INTER_CUBIC)
+        self._img = cv2.resize(self._img, None,
+                               fx=x_factor, fy=y_factor,
+                               interpolation=cv2.INTER_CUBIC)
 
     def apply_and_save(self, result_path: str):
         """
         apply changes and save at path relative to current directory
         """
-        cv2.imwrite(f'./{result_path}', self.img)
+        cv2.imwrite(f'./{result_path}', self._img)
 
     def _get_rotation_point(self, rotation_pt: RotationPoint) -> Tuple[int, int]:
         if rotation_pt is RotationPoint.CENTER:
-            return self.cols / 2, self.rows / 2
+            return self._cols / 2, self._rows / 2
         elif rotation_pt is RotationPoint.TOP_LEFT:
             return 0, 0
         elif rotation_pt is RotationPoint.TOP_RIGHT:
-            return self.cols, 0
+            return self._cols, 0
         elif rotation_pt is RotationPoint.BOT_LEFT:
-            return 0, self.rows
+            return 0, self._rows
         elif rotation_pt is RotationPoint.BOT_RIGHT:
-            return self.cols, self.rows
+            return self._cols, self._rows
         else:
             raise ValueError("unexpected rotation point!")
 
 
 def main():
-    editor = ImgEditor(SOURCE_IMG_PATH)
-    editor.rotate(RotationPoint.CENTER, 10)
-    editor.shrink(x_factor=0.7, y_factor=0.7)
-    editor.apply_and_save(RESULT_IMG_PATH)
+    img = ImgEditor(SOURCE_IMG_PATH)
+    img.rotate(RotationPoint.CENTER, 10)
+    img.shrink(x_factor=0.7, y_factor=0.7)
+    img.apply_and_save(RESULT_IMG_PATH)
 
 
 if __name__ == '__main__':
