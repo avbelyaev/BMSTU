74be3ea98c1e3cc78cb54300847df2480e5323e7
anthony <antonsdl@gmail.com>
2019-09-16 23:12:38 +0300

    skipping 1st step of simplex

--- _masters_/decision-theory/lab1-simplexx/simplexx.py
+++ _masters_/decision-theory/lab1-simplexx/simplexx.py
@@ -22,10 +22,38 @@ def add_fictional_vars(a: np.matrix, signs: np.matrix, m_rows: int) -> np.matrix
     return a
 
 
+def step_1_solve():
+    pass
+
+
+def step_2_opt(matr: list, b: list):
+    pass
+
+
+def simlex(matr: np.matrix, b: np.matrix):
+    matr = matr.astype(np.float).tolist()
+    b = b.transpose().astype(np.float).tolist()[0]
+
+    all_free_vars_are_positive = True
+    for b_i in b:
+        if b_i < 0:
+            all_free_vars_are_positive = False
+            break
+
+    if all_free_vars_are_positive:
+        result = step_2_opt(matr, b)
+    else:
+        step_1_solve()
+        result = step_2_opt()
+
+    return result
+
+
 def main():
-    matr = np.matrix([[1, 1, '=', 10],
-                      [-2, 3, '<=', -5],
-                      [7, -4, '>=', 6]])
+    matr = np.matrix([[2, 1, 1, '<=', 4],
+                      [1, 2, 0, '<=', 6],
+                      [0, 0.5, 1, '<=', 2]])
+    c = [2, 8, 3]
     b = matr[:, -1]
     signs = matr[:, -2]
     a = matr[:, :-2]
@@ -36,8 +64,11 @@ def main():
     print(matr)
 
     print('--- fictional vars added ---')
-    reshaped = add_fictional_vars(a, signs, m_rows)
-    print(reshaped)
+    fictionals = add_fictional_vars(a, signs, m_rows)
+    print(fictionals, b)
+
+    result = simlex(fictionals, b)
+    print(result)
 
 
 if __name__ == '__main__':
