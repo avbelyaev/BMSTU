7f59b0927985cbf14ffa86745c84470c4cec6087
anthony <antonsdl@gmail.com>
2019-09-21 17:07:51 +0300

    reapproximating

--- _masters_/ml/square-contours/contours.py
+++ _masters_/ml/square-contours/contours.py
@@ -13,11 +13,17 @@ COLORS = ['white', 'green', 'purple', 'black', 'blue',
 
 class Figure:
     def __init__(self, contour):
-        self.contour = contour
-        self.peri = cv2.arcLength(contour, closed=True)
-        self.area = cv2.contourArea(contour)
-        self.approx = cv2.approxPolyDP(contour, epsilon=0.001 * self.peri, closed=True)
         self.color_name = COLORS[random.randint(0, len(COLORS) - 1)]
+        self.orginal_contour = contour
+        # initial approximation
+        self.area = cv2.contourArea(contour)
+        self.peri = cv2.arcLength(contour, closed=True)
+        self.approx = cv2.approxPolyDP(contour, epsilon=0.001 * self.peri, closed=True)
+
+    def reapprox(self, eps: float=0.001):
+        self.approx = cv2.approxPolyDP(self.approx, epsilon=eps * self.peri, closed=True)
+        self.peri = cv2.arcLength(self.approx, closed=True)
+        self.area = cv2.contourArea(self.approx)
 
     @property
     def color_bgr(self) -> tuple:
@@ -28,21 +34,22 @@ class Figure:
         return self.area - other.area < 100
 
     def __str__(self):
-        return f'{self.color_name}: peri: {self.peri}, area: {self.area}, ' \
-               f'points: {len(self.contour)} -> {len(self.approx)}'
+        return f'{self.color_name}: area: {self.area}\n' \
+               f'   peri: {self.peri}\n' \
+               f'   points: {len(self.orginal_contour)} -> {len(self.approx)}'
 
 
 def random_color_name():
     return 'green'
 
 
-def draw_squares(image, figures: 'List[Figure]'):
+def draw_squares(img, figures: 'List[Figure]'):
     for fig in figures:
         draw_all_contours = -1
         thickness = 2
-        cv2.drawContours(image, [fig.contour], draw_all_contours, fig.color_bgr, thickness)
+        cv2.drawContours(img, [fig.approx], draw_all_contours, fig.color_bgr, thickness)
 
-    cv2.imwrite(f'./{RESULT_IMG_PATH}', image)
+    cv2.imwrite(f'./{RESULT_IMG_PATH}', img)
 
 
 def already_exists(new_figure: Figure, figures: list):
@@ -51,6 +58,13 @@ def already_exists(new_figure: Figure, figures: list):
             return True
     return False
 
+
+def draw_figures(figures: list):
+    print('---')
+    [print(fig) for fig in figures]
+
+    # sharpenKernel = np.array([[-1, -1, -1], [-1, 10, -1], [-1, -1, -1]])
+    # img = cv2.filter2D(img, -1, sharpenKernel)
 
 def main():
     img = cv2.imread(SOURCE_IMG_PATH)
@@ -66,7 +80,7 @@ def main():
 
     thresh = 250
     # img = cv2.threshold(img, thresh, 255, cv2.THRESH_MASK)[1]
-    edged = cv2.Canny(img, threshold1=40, threshold2=200)
+    img = cv2.Canny(img, threshold1=40, threshold2=200)
 
     # img = cv2.medianBlur(img, 5)[1]
 
@@ -74,14 +88,23 @@ def main():
     # img = cv2.erode(img, kernel, iterations=1)
     # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     figures = []
     for ctr in contours:
         fig = Figure(ctr)
-
         if not already_exists(fig, figures):
             figures.append(fig)
-            print(fig)
+
+    # sort by perimeter DESC to find biggest one
+    figures.sort(key=lambda fig: fig.area, reverse=True)
+    biggest = figures[0].reapprox(eps=0.05)
+
+    draw_figures(figures)
+
+    smallest_1 = figures[-1].reapprox(eps=0.01)
+    smallest_2 = figures[-2].reapprox(eps=0.07)
+
+    draw_figures(figures)
 
     draw_squares(original, figures)
 
