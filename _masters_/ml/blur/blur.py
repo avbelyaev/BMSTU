45738213c973f285dddfa841220279f167f3e8af
anthony <antonsdl@gmail.com>
2019-09-28 01:24:20 +0300

    added glvn

--- _masters_/ml/blur/blur.py
+++ _masters_/ml/blur/blur.py
@@ -7,15 +7,16 @@ from _masters_.ml.square_contours.contours import Point
 # https://vk.com/doc1164151_516201556?hash=d3ac940079dcba822a&dl=37b24c9319a7ddb58b
 
 SOURCE_IMG_PATH = 'original.png'
-RESULT_IMG_PATH = 'res.png'
+RESULT_TENG_IMG_PATH = 'teng-res.png'
+RESULT_GLVN_IMG_PATH = 'glvn-res.png'
 
 # размер окна, проходя которым по фрагменту, проверяем его заблюренность
-WINDOW_HEIGHT = 30
-WINDOW_WIDTH = 30
+WINDOW_HEIGHT = 3
+WINDOW_WIDTH = 3
 
 # фрагменты, на которые бьется изображение
-FRAGMENT_HEIGHT = 40
-FRAGMENT_WIDTH = 50
+FRAGMENT_HEIGHT = 30
+FRAGMENT_WIDTH = 30
 
 SOBEL_X = [[-1, 0, 1],
            [-2, 0, 2],
@@ -29,10 +30,57 @@ class Fragment:
     def __init__(self, top_left: Point, bot_right: Point, img: np.ndarray):
         self.top_left = top_left
         self.bot_right = bot_right
-        part = img[top_left.y: bot_right.y, top_left.x: bot_right.x]
-        self.img = part
+        self.img = img[top_left.y: bot_right.y, top_left.x: bot_right.x]
+        self.height, self.width = self.img.shape[:2]
+        self.teng = None    # for memoization
+        self.glvn = None    # for memoization
+        self.lvs = dict()
 
-    def local_variance_at(self, m: int, n: int) -> float:
+    def tenengrad(self) -> float:
+        if self.teng is not None:
+            return self.teng
+        G_x = convolve2d(self.img, SOBEL_X)
+        G_y = convolve2d(self.img, SOBEL_Y)
+        S = np.sqrt(np.square(G_x) + np.square(G_y))
+
+        threshold = np.max(S) * 0.66
+        res = np.where(S > threshold)
+
+        self.teng = np.sum(np.square(res))
+        return self.teng
+
+    def grey_level_local_variance(self) -> float:
+        if self.glvn is not None:
+            return self.glvn
+        lv = 0
+        M = self.width
+        N = self.height
+        m = 0
+        while m < M - WINDOW_HEIGHT:
+            n = 0
+            while n < N - WINDOW_WIDTH:
+                lv += self._local_variance_at(n, m)
+                n += 1
+            m += 1
+
+        lv_mean = lv / (M * N)
+
+        m = 0
+        glvn = 0
+        while m < M - WINDOW_HEIGHT:
+            n = 0
+            while n < N - WINDOW_WIDTH:
+                lv = self._local_variance_at(n, m)
+                glvn += (lv - lv_mean) ** 2
+                n += 1
+            m += 1
+
+        self.glvn = glvn / (M * N)
+        return self.glvn
+
+    def _local_variance_at(self, m: int, n: int) -> float:
+        if (m, n) in self.lvs:
+            return self.lvs[(m, n)]
         summ = 0
         i = 0
         while i < WINDOW_WIDTH:
@@ -46,20 +94,13 @@ class Fragment:
                 summ += (I - I_mean) ** 2
                 j += 1
             i += 1
-        return summ / (WINDOW_HEIGHT * WINDOW_WIDTH)
 
-    def tenengrad(self) -> float:
-        G_x = convolve2d(self.img, SOBEL_X)
-        G_y = convolve2d(self.img, SOBEL_Y)
-        S = np.sqrt(np.square(G_x) + np.square(G_y))
-
-        threshold = np.max(S) * 0.63
-        res = np.where(S > threshold)
-        return np.sum(np.square(res))
+        self.lvs[(m, n)] = summ / (WINDOW_HEIGHT * WINDOW_WIDTH)
+        return self.lvs[(m, n)]
 
     def draw(self, img: np.ndarray):
         bgr_orange_color = (0, 128, 255)
-        return cv2.rectangle(img,
+        cv2.rectangle(img,
                       pt1=(self.top_left.x, self.top_left.y),
                       pt2=(self.bot_right.x, self.bot_right.y),
                       color=bgr_orange_color, thickness=1)
@@ -107,16 +148,28 @@ def main():
 
     # detect blurred pieces with tenengrad
     tenengrads = list(map(lambda f: f.tenengrad(), fragments))
-    teng_thresh = max(tenengrads) * 0.48
+    teng_thresh = max(tenengrads) * 0.66
 
     for fragment in fragments:
         if fragment.tenengrad() > teng_thresh:
             fragment.draw(original1)
 
-    cv2.imwrite(f'./teng-{RESULT_IMG_PATH}', original1)
+    cv2.imwrite(f'./{RESULT_TENG_IMG_PATH}', original1)
 
     # detect blur with GLVN
+    glvns = []
+    i = 0
+    for f in fragments:
+        glvns.append(f.grey_level_local_variance())
+        print(f'{i}/{len(fragments)}: {f.grey_level_local_variance()}')
+        i += 1
+    glvn_thresh = max(glvns) * 0.001
 
+    for fragment in fragments:
+        if fragment.grey_level_local_variance() < glvn_thresh:
+            fragment.draw(original2)
+
+    cv2.imwrite(f'./{RESULT_GLVN_IMG_PATH}', original2)
 
 
 if __name__ == '__main__':
