1cd26ce92d212e909b30de7ac04e9f6e65239f5d
anthony <antonsdl@gmail.com>
2019-09-27 23:14:20 +0300

    blurring image with tenengrad

--- _masters_/ml/blur/blur.py
+++ _masters_/ml/blur/blur.py
@@ -1,24 +1,37 @@
 import cv2
 import numpy as np
+from scipy.signal import convolve2d
 
 from _masters_.ml.square_contours.contours import Point
+
 # https://vk.com/doc1164151_516201556?hash=d3ac940079dcba822a&dl=37b24c9319a7ddb58b
 
-SOURCE_IMG_PATH = 'blurred.jpg'
+SOURCE_IMG_PATH = 'blurred.png'
+# SOURCE_IMG_PATH = 'blurred2.jpg'
 RESULT_IMG_PATH = 'res.png'
 
+# размер окна, проходя которым по фрагменту, проверяем его заблюренность
 WINDOW_HEIGHT = 30
 WINDOW_WIDTH = 30
 
-FRAGMENT_HEIGHT = 80
-FRAGMENT_WIDTH = 100
+# фрагменты, на которые бьется изображение
+FRAGMENT_HEIGHT = 40
+FRAGMENT_WIDTH = 50
+
+SOBEL_X = [[-1, 0, 1],
+           [-2, 0, 2],
+           [-1, 0, 1]]
+SOBEL_Y = [[1, 2, 1],
+           [0, 0, 0],
+           [-1, -2, -1]]
 
 
 class Fragment:
     def __init__(self, top_left: Point, bot_right: Point, img: np.ndarray):
         self.top_left = top_left
         self.bot_right = bot_right
-        self.img = img[top_left.x: bot_right.x, top_left.y: bot_right.y]
+        part = img[top_left.y: bot_right.y, top_left.x: bot_right.x]
+        self.img = part
 
     def local_variance_at(self, m: int, n: int) -> float:
         summ = 0
@@ -35,6 +48,22 @@ class Fragment:
                 j += 1
             i += 1
         return summ / (WINDOW_HEIGHT * WINDOW_WIDTH)
+
+    def tenengrad(self) -> float:
+        G_x = convolve2d(self.img, SOBEL_X)
+        G_y = convolve2d(self.img, SOBEL_Y)
+        S = np.sqrt(np.square(G_x) + np.square(G_y))
+
+        threshold = np.max(S) * 0.63
+        res = np.where(S > threshold)
+        return np.sum(np.square(res))
+
+    def draw(self, img: np.ndarray):
+        bgr_color = (0, 0, 255)
+        return cv2.rectangle(img,
+                      pt1=(self.top_left.x, self.top_left.y),
+                      pt2=(self.bot_right.x, self.bot_right.y),
+                      color=bgr_color, thickness=1)
 
     def __repr__(self):
         return self.__str__()
@@ -56,31 +85,31 @@ def crop(img) -> 'list[Fragment]':
     fragments = []
     img_h, img_w = img.shape[:2]
     y = 0
-    while y < img_w:
+    while y < img_h:
         x = 0
-        while x < img_h:
+        while x < img_w:
             fragment = Fragment(top_left=Point(x, y),
                                 bot_right=Point(x + FRAGMENT_WIDTH, y + FRAGMENT_HEIGHT),
                                 img=img)
             fragments.append(fragment)
-            x += FRAGMENT_HEIGHT
-        y += FRAGMENT_WIDTH
+            x += FRAGMENT_WIDTH
+        y += FRAGMENT_HEIGHT
     return fragments
 
 
 def main():
     img = cv2.imread(SOURCE_IMG_PATH, cv2.IMREAD_GRAYSCALE)
     original = cv2.imread(SOURCE_IMG_PATH)
-    # img = cv2.resize(img, )
     fragments = crop(img)
 
-    print(fragments)
+    # detect blur with tenengrad
+    tenengrads = list(map(lambda fragment: fragment.tenengrad(), fragments))
+    teng_thresh = max(tenengrads) * 0.48
 
-    frag = fragments[3]
-    lv = frag.local_variance_at(0, 0)
-
-    cv2.rectangle(original, (frag.top_left.x, frag.top_left.y),
-                  (frag.bot_right.x, frag.bot_right.y), (0, 0, 255), thickness=2)
+    for fragment in fragments:
+        teng = fragment.tenengrad()
+        if teng > teng_thresh:
+            fragment.draw(original)
 
     cv2.imwrite(f'./{RESULT_IMG_PATH}', original)
 
