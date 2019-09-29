42f972957d4ac91ffef3db9fcf5f6de7f6681fb3
anthony <antonsdl@gmail.com>
2019-09-29 12:15:54 +0300

    added svm api stub

--- _masters_/ml/svm/svm.py
+++ _masters_/ml/svm/svm.py
@@ -3,6 +3,23 @@ import matplotlib.pyplot as plt
 
 DATASET_SIZE = 100
 
+CLAZZ_1 = -1
+CLAZZ_2 = +1
+
+
+class SVM:
+    def __init__(self):
+        pass
+
+    def fit(self, data: np.ndarray):
+        all_data = []
+        all_data.extend(data[CLAZZ_1])
+        all_data.extend(data[CLAZZ_2])
+        print('asd')
+
+    def predict(self, data: np.ndarray):
+        pass
+
 
 def main():
     center1 = (50, 60)
@@ -15,9 +32,29 @@ def main():
     x2 = np.random.uniform(center2[0], center2[0] + distance, size=(DATASET_SIZE,))
     y2 = np.random.normal(center2[1], distance, size=(DATASET_SIZE,))
 
-    plt.scatter(x1, y1)
-    plt.scatter(x2, y2)
-    plt.show()
+    # plt.scatter(x1, y1)
+    # plt.scatter(x2, y2)
+    # plt.show()
+
+    clazz1_points = []
+    for i in range(DATASET_SIZE):
+        x = x1[i]
+        y = y1[i]
+        clazz1_points.append([x, y])
+
+    clazz2_points = []
+    for i in range(DATASET_SIZE):
+        x = x2[i]
+        y = y2[i]
+        clazz2_points.append([x, y])
+
+    marked_up_data = {
+        CLAZZ_1: clazz1_points,
+        CLAZZ_2: clazz2_points
+    }
+
+    svm = SVM()
+    svm.fit(marked_up_data)
 
 
 if __name__ == '__main__':
