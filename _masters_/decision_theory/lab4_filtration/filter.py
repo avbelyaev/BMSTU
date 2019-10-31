6f9bea0ce68d7427e7fbb9b68c252c86214805eb
anthony <antonsdl@gmail.com>
2019-10-31 11:38:04 +0300

    the rest of work

--- _masters_/decision_theory/lab4_filtration/filter.py
+++ _masters_/decision_theory/lab4_filtration/filter.py
@@ -1,13 +1,18 @@
 import math
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
-FUNC = lambda x: math.sin(x) + 0.5  # исходая функция
-K_STEPS = 100  # количество отсчетов
-R_WINDOW_SIZE = 3  # размер скользящего окна
-SIGMA_NOISE_AMPLITUDE = 0.25  # разброс значений зашемленного сигнала
-X_MIN = 0  # интервал от
-X_MAX = math.pi  # интервал до
+# @formatter:off
+SIGNAL_FUNC = lambda x: math.sin(x) + 0.5       # исходая функция
+K_STEPS = 100                                   # количество отсчетов
+R_WINDOW_SIZE = 5                               # размер скользящего окна
+SIGMA_NOISE_AMPLITUDE = 0.25                    # разброс значений зашемленного сигнала
+X_MIN = 0                                       # интервал от
+X_MAX = math.pi                                 # интервал до
+NAIVE_CENTRAL_ALPHA = 0.5                       # наивное значение alpha
+NAIVE_LAMBDA = 0.5                              # наивное значение лямбды
+# @formatter:on
 
 
 # шум с нормальным распределением
@@ -16,81 +21,165 @@ def uniform_noise(signal: float) -> float:
     return signal + noise
 
 
-# Среднее гармоническое
-# def harmonic_mean(k: int, m: int, alphas: list) -> float:
+# критерий зашумленности сигнала, метрика чебышева
+# aka максимальная разность двух рядомстоящих элементов
+def noisy_criteria(signal: list) -> float:
+    max_diff = -999999
+    for k in range(1, K_STEPS):
+        diff = abs(signal[k] - signal[k - 1])
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
 
-# Чебышева
+
+# критейрий близости сигналов, метрика чебышева
+def closeness_criteria(signal1: list, signal2: list) -> float:
+    max_diff = -999999
+    for k in range(0, K_STEPS):
+        diff = abs(signal1[k] - signal2[k])
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
+
+
+# метрика расстояния до идеальной точки, метрика чебышева
+def ideal_point_distance_metric(omega: float, delta: float) -> float:
+    return max(omega, delta)
 
 
 class Signal:
-    def __init__(self, func: callable, xmin: float, xmax: float, steps: int,
-                 window_size: int):
+    def __init__(self, func: callable):
         self.signal_function = func
-        self.x_min = xmin
-        self.x_max = xmax
-        self.steps = steps
-        self.window_size = window_size
-        self.m = (window_size - 1) // 2
+        self.m = (R_WINDOW_SIZE - 1) // 2
+        self.central_alpha = NAIVE_CENTRAL_ALPHA
+        self.lambda_weight = NAIVE_LAMBDA
 
     @property
-    def xs(self) -> list:
+    def xs(self):
         xs = []
-        x_curr = self.x_min
-        step = (self.x_max - self.x_min) / self.steps
-        while x_curr <= self.x_max:
+        x_curr = X_MIN
+        step = (X_MAX - X_MIN) / K_STEPS
+        while x_curr <= X_MAX:
             xs.append(x_curr)
             x_curr += step
         return xs
 
-    @property
     def clear_signal(self) -> list:
         return list(map(lambda x: self.signal_function(x), self.xs))
 
-    @property
     def noisy_signal(self) -> list:
-        return list(map(lambda signal: uniform_noise(signal), self.clear_signal))
+        return list(map(lambda signal: uniform_noise(signal), self.clear_signal()))
 
-    @property
-    def filtered_signal(self) -> list:
-        fs = self.noisy_signal
-        alphas = [0.4, 0.2, 0.4]  # TODO wtf is this
-
+    def filtered_signal(self, signal: list) -> list:
         filtered = []
-        filtered.extend(self.clear_signal[:self.m])
+        filtered.extend(signal[:self.m])  # TODO по краям - изначальный или шумный?
+
+        # @formatter:off
+        if 3 == R_WINDOW_SIZE:
+            alphas = [
+                (1 - self.central_alpha) / 2,       # i - 1
+                self.central_alpha,                 # i
+                (1 - self.central_alpha) / 2        # i + 1
+            ]
+        elif 5 == R_WINDOW_SIZE:
+            alphas = [
+                (1 - self.central_alpha) / 4,       # i - 2
+                (1 - self.central_alpha) / 4,       # i - 1
+                self.central_alpha,                 # i
+                (1 - self.central_alpha) / 4,       # i + 1
+                (1 - self.central_alpha) / 4        # i + 2
+            ]
+        else:
+            raise ValueError('invalid window size')
+        # @formatter:on
 
         k = self.m
-        while k < self.steps - self.m:
-            window_sum = sum([alphas[0] / fs[k - 1],
-                              alphas[1] / fs[k],
-                              alphas[2] / fs[k + 1]])
+        while k < K_STEPS - self.m:
+            # Среднее гармоническое
+            if 3 == R_WINDOW_SIZE:
+                window_sum = sum([alphas[0] / signal[k - 1],
+                                  alphas[1] / signal[k],
+                                  alphas[2] / signal[k + 1]])
+            else:
+                window_sum = sum([alphas[0] / signal[k - 2],
+                                  alphas[1] / signal[k - 1],
+                                  alphas[2] / signal[k],
+                                  alphas[3] / signal[k + 1],
+                                  alphas[4] / signal[k + 2]])
             harmonic_mean_value = 1 / window_sum
             filtered.append(harmonic_mean_value)
             k += 1
-        filtered.extend((self.clear_signal[k:]))
+        filtered.extend(signal[k:])
         return filtered
 
-    def plot(self):
-        plt.subplot(311)
-        plt.title('Clear signal', fontdict={'size': 10})
-        plt.plot(self.xs, self.clear_signal, c='green')
+    def linear_convolution_of_criteria(self):
+        omega = noisy_criteria(self.filtered_signal(self.noisy_signal()))
+        delta = closeness_criteria(self.filtered_signal(self.noisy_signal()), self.clear_signal())
+        return self.lambda_weight * omega + (1 - self.lambda_weight) * delta
 
-        plt.subplot(312)
-        plt.title('Noisy signal', fontdict={'size': 10})
-        plt.plot(self.xs, self.noisy_signal, c='red')
+    def optimize_alphas(self):
+        criterias = dict()
+        # разбиваем отрезок на N частей
+        alpha_range = np.linspace(start=0, stop=1, num=20)[1:-1]
+        for alpha in alpha_range:
+            self.central_alpha = alpha
+            # пересчитываем значение j
+            j = self.linear_convolution_of_criteria()
+            criterias[j] = alpha
 
-        plt.subplot(313)
-        plt.title('Filtered signal', fontdict={'size': 10})
-        plt.plot(self.xs, self.filtered_signal, c='blue')
+        # находим альфу,которой соответствует минимум функции J и делаем ее текущей
+        minimal_by_j_criteria = sorted(criterias.keys())[0]
+        self.central_alpha = criterias[minimal_by_j_criteria]
 
-        plt.tight_layout()
-        plt.savefig('plot.png')
+    def optimize_lambdas(self):
+        def dist():
+            omega = noisy_criteria(self.filtered_signal(self.noisy_signal()))
+            delta = closeness_criteria(self.filtered_signal(self.noisy_signal()), self.clear_signal())
+            return ideal_point_distance_metric(omega, delta)
+
+        criterias = dict()
+        lambdas_range = [1 / l for l in range(1, 20)]
+        for lambda_value in lambdas_range:
+            self.lambda_weight = lambda_value
+            # пересчитываем значение дистанции до опт. точки
+            d = dist()
+            criterias[d] = lambda_value
+
+        # находим лямбду
+        minimal_by_distance = sorted(criterias.keys())[0]
+        self.lambda_weight = criterias[minimal_by_distance]
+
+
+def main():
+    s = Signal(SIGNAL_FUNC)
+
+    # plot original
+    plt.subplot(411)
+    plt.title('Clear signal', fontdict={'size': 10})
+    plt.plot(s.xs, s.clear_signal(), c='orange')
+
+    # plot noisy
+    plt.subplot(412)
+    plt.title('Noisy signal', fontdict={'size': 10})
+    plt.plot(s.xs, s.noisy_signal(), c='red')
+
+    # plot naive filtered
+    plt.subplot(413)
+    plt.title('Naive filtered signal', fontdict={'size': 10})
+    plt.plot(s.xs, s.filtered_signal(signal=s.noisy_signal()), c='green')
+
+    # find optimal alphas, lambdas
+    s.optimize_alphas()
+    s.optimize_lambdas()
+
+    # plot with optimal alphas
+    plt.subplot(414)
+    plt.title('Optimally filtered signal', fontdict={'size': 10})
+    plt.plot(s.xs, s.filtered_signal(signal=s.noisy_signal()), c='blue')
+
+    plt.tight_layout()
+    plt.savefig('plot.png')
 
 
 if __name__ == '__main__':
-    Signal(
-        func=FUNC,
-        xmin=X_MIN,
-        xmax=X_MAX,
-        steps=K_STEPS,
-        window_size=R_WINDOW_SIZE
-    ).plot()
+    main()
